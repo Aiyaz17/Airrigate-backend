5c2b4be3246636f2ea99d89ae48f873491f24308
Aiyaz Qureshi <qureshi.aiyaz123@gmail.com>
2023-02-06 13:47:16 +0530

    removed lfs

--- app.py
+++ app.py
@@ -12,10 +12,23 @@ import requests
 
 app = Flask(__name__)
 CORS(app)
-crop_model = pickle.load(open('.\\models\\crop.pickle', 'rb'))
-fertilizer_model = pickle.load(open('.\\models\\fertilizer.pickle', 'rb'))
-yield_model = pickle.load(open('.\\models\\yield.pickle', 'rb'))
 
+# with open('.\\models\\crop.pickle', 'rb') as f:
+#     crop_model = pickle.load(f)
+# with open('.\\models\\fertilizer.pickle', 'rb') as f:
+#     fertilizer_model = pickle.load(f)
+# with open('.\\models\\yield.pickle', 'rb') as f:
+#     yield_model = pickle.load(f)
+
+# windows
+# crop_model = pickle.load(open('.\\models\\crop.pickle', 'rb'))
+# fertilizer_model = pickle.load(open('.\\models\\fertilizer.pickle', 'rb'))
+# yield_model = pickle.load(open('.\\models\\yield.pickle', 'rb'))
+
+# unix
+crop_model = pickle.load(open('./models/crop.pickle', 'rb'))
+fertilizer_model = pickle.load(open('./models/fertilizer.pickle', 'rb'))
+yield_model = pickle.load(open('./models/yield.pickle', 'rb'))
 @app.route("/")
 def home():
     # return fetchClimateApi("usa")
@@ -124,7 +137,7 @@ def predictYield():
 @app.route("/detect-disease",methods=["POST"])
 def detectDisease():
     img = request.files['image']
-    link = "del."+imghdr.what(img)
+    link = "temp."+imghdr.what(img)
     img.save(link)
     # print(img)
     # disease_model = tf.keras.models.load_model("pathology-detector-optim")
